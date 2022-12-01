4a6f3aa1e3c245d1512830848c109280c98bc8cb
RebikHub <rebikovlk@mail.ru>
2022-12-01 14:17:49 +0200

    for appveyor

--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { ReactElement, SyntheticEvent, useEffect } from "react";
 import { ReactNode } from "react";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
